63c0b090bdac837bd7d85206bcf6ae4516fe806c
Tao242dn <tao242dn@gmail.com>
2024-08-03 08:38:32 +0700

    Update init.db.js and README.md

--- src/dbs/init.db.js
+++ src/dbs/init.db.js
@@ -10,17 +10,19 @@ const {
 const connectString = `mongodb+srv://${username}:${password}@${cluster}.kipqpva.mongodb.net/`;
 
 class Database {
-  constructor() {}
+  constructor() {
+    this.connection = null; // Store the connection object
+  }
 
   // Connect Db
   async connect(type = `mongodb`) {
-    if (1 === 1) {
+    if (process.env.NODE_ENV === "dev") { // Only enable for debugging in non-production env
       mongoose.set("debug", true);
       mongoose.set("debug", { color: true });
     }
 
     try {
-      await mongoose.connect(connectString, { maxPoolSize: 50 });
+      this.connection = await mongoose.connect(connectString, { maxPoolSize: 50 });
       console.log(`Connected to MongoDB successfully.`);
     } catch (err) {
       console.error(`Error connecting to MongoDB: ${err}`);
@@ -31,8 +33,13 @@ class Database {
   // Disconnect Db
   async disconnect() {
     try {
-      await mongoose.disconnect();
-      console.log(`Disconnected from MongoDB.`);
+      if (this.connection) { // Only disconnect if a connection exits 
+        await this.connection.disconnect();
+        this.connection = null; // Reset the connection object
+        console.log(`Disconnected from MongoDB.`);
+      } else {
+        console.log(`No active MongoDB connection to disconnect.`);
+      }
     } catch (err) {
       console.error(`Error disconnecting from MongoDB: ${err}`);
     }
