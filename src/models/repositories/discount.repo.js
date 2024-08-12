59d770e21baa1f05ee19af65129a8f54ea409c1f
Tao242dn <tao242dn@gmail.com>
2024-08-12 23:57:08 +0700

    Update discount.repo.js

--- src/models/repositories/discount.repo.js
+++ src/models/repositories/discount.repo.js
@@ -1,35 +1,47 @@
-import { getSelectData, unGetSelectData } from "../../utils/index.js";
+import { getSelectData, unGetSelectData } from '../../utils/index.js';
 
-export const findAllDiscountCodesUnSelect = async({
-    limit = 50, page = 1, sort = 'ctime',
-    filter, unSelect, model
+export const findAllDiscountCodesUnSelect = async ({
+  limit = 50,
+  page = 1,
+  sort = 'ctime',
+  filter,
+  unSelect,
+  model,
 }) => {
-    const skip = limit * (page - 1);
-    const sortBy = sort === 'ctime' ? { _id: -1 } : { _id: 1 };
-    const products = await model
-      .find(filter)
-      .sort(sortBy)
-      .skip(skip)
-      .limit(limit)
-      .select(unGetSelectData(unSelect))
-      .lean();
-  
-    return products;
-}
+  const skip = limit * (page - 1);
+  const sortBy = sort === 'ctime' ? { _id: -1 } : { _id: 1 };
+  const products = await model
+    .find(filter)
+    .sort(sortBy)
+    .skip(skip)
+    .limit(limit)
+    .select(unGetSelectData(unSelect))
+    .lean();
 
-export const findAllDiscountCodesSelect = async({
-    limit = 50, page = 1, sort = 'ctime',
-    filter, select, model
+  return products;
+};
+
+export const findAllDiscountCodesSelect = async ({
+  limit = 50,
+  page = 1,
+  sort = 'ctime',
+  filter,
+  select,
+  model,
 }) => {
-    const skip = limit * (page - 1);
-    const sortBy = sort === 'ctime' ? { _id: -1 } : { _id: 1 };
-    const products = await model
-      .find(filter)
-      .sort(sortBy)
-      .skip(skip)
-      .limit(limit)
-      .select(getSelectData(select))
-      .lean();
-  
-    return products;
-}
+  const skip = limit * (page - 1);
+  const sortBy = sort === 'ctime' ? { _id: -1 } : { _id: 1 };
+  const products = await model
+    .find(filter)
+    .sort(sortBy)
+    .skip(skip)
+    .limit(limit)
+    .select(getSelectData(select))
+    .lean();
+
+  return products;
+};
+
+export const checkDiscountExits = async (model, filter) => {
+  return await model.findOne(filter).lean();
+};
